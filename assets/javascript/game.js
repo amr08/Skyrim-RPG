c1d1d871450df56bf7a2175380b7095f366c4c1e
Andrea R <amrdev08@gmail.com>
2016-08-09 11:13:51 -0400

    game play

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 $('.well').css("background", "grey");
 
 
-
+//Player selection
 $('#firstRow a').click(function(event) {
 event.stopPropagation();
 var self = $(this);
@@ -28,52 +28,114 @@ $('#secondRow a').click(function(event) {
     $(this).removeClass("enemy");
     $(this).removeClass("character");
     $(".btn.defender").detach().appendTo("#thirdRow");
-    $(".btn.character.enemy").detach().appendTo("#firstRow");
+    $(".btn.character.enemy").detach().appendTo("#firstRow").removeClass("enemy");
+
  });
 });
-
-//$('.btn-danger').on("click", function() {
-
-
-//}
-
-var hp = [150,160,170,200];
-
-var attackPower = [25,50,30,10];
-
-var counterAttack = [10, 30,5,20];
-
-var counter=0
+//end player selection
 
 
 
- 
- for(var i = 0; i < hp.length; i++) {
-    console.log(hp[i]);
+
+
+//Storing Character Stats
+
+var char1 = {
+
+    hp: 150,
+    attackPower: 5,
+    counterAttack: 10,
 }
 
-     //$("#char1").attr('data-num', hp[i]);
-     //$("#char2").attr('data-num', hp[i]);
-    //console.log($("#char1").text(hp[i]));
-   
 
+var char2 = {
+    hp: 200,
+    attackPower: 10,
+    counterAttack: 15,
+
+}
+
+var char3 = {
+    hp: 170,
+    attackPower: 7,
+    counterAttack: 10,
+}
+
+var char4 = {
+    hp: 160,
+    attackPower: 10,
+    counterAttack: 9,
+};
+
+
+//Assigning character stats to DOM
+    $("#char1").attr('data-num', char1.hp, char1.attackPower, char1.counterAttack);
+    $("#char1 p").text("HP " + char1.hp);
+
+    $("#char2").attr('data-num', char2.hp, char2.attackPower, char2.counterAttack);
+    $("#char2 p").text("HP " + char2.hp);
+
+    $("#char3").attr('data-num', char3.hp, char3.attackPower, char3.counterAttack);
+    $("#char3 p").text("HP " + char3.hp);
+
+    $("#char4").attr('data-num', char4.hp, char4.attackPower, char4.counterAttack);
+    $("#char4 p").text("HP " + char4.hp);
+
+  //
+
+
+function battleOne() {
+
+               $("#char1 p").text("HP " + (char1.hp - char2.attackPower));
+               $("#char2 p").text("HP " + (char2.hp - char1.attackPower));
+                 
+                 if ($(".btn.character") == $("#char1")) {
+
+                    $(".battleStats").text("You attacked enemy for " + char1.attackPower + " damage. Enemy attacked you back for " + char2.attackPower + " damage");
+                     }
+
+                     else {
+                        $(".battleStats").text("You attacked enemy for " + char2.attackPower + " damage. Enemy attacked you back for " + char1.attackPower + " damage");
+                     }
+
+};
+
+
+
+//Fight sequence
+//$('.btn-danger').on("click", function() {
+
+   //var fight = $(".character").val(char1.hp + attackPower - counterAttack);
+   //console.log(fight);
+
+    
     //});
+//.btn.defender fights .btn.character
 
 
+
+
+
+//end
+$('.btn-danger').on("click", function() {
+
+    if (char1 === $(".btn.character") || $(".btn.defender") && char2 === $("btn.defender") || $(".btn.character")) {
+       battleOne();
+    };
+
+});
 
 
 
 
     
-
-//$('#btn-danger').on("click"), function() {
-    //$('a:first').val() attackPower
-//}
+//restart
 
 
-
-if (hp === 0) {
-location.reload();
+function restart() {
+    if (hp === 0) {
+    location.reload();
+}
     //restart
 };
 
